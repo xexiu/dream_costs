dbc66ce3a0f49bf56f1b6d04ca7ca1c99062fdae
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-17 19:38:49 +0100

    Final Changes ALL v2

--- public/js/register.js
+++ public/js/register.js
@@ -1,18 +1,30 @@
 $(document).ready(function(){
+  $('.openRegister').click(function(e){
+     e.stopPropagation();
+      $('.register').css({
+      'display':'block'
+      });
+      $('.register').animate({
+      'margin-right':'0',
+      }, 'slow');
+  });
 
-$('.openRegister').click(function(e){
-   e.stopPropagation();
- $('.register').css({
-  'display':'block'
- });
- $('.register').animate({
-  'margin-right':'0',
- }, 'slow');
-});
+  $('.register').click(function(e){
+    e.stopPropagation();
+  });
 
-$(document).click(function(e){
+  $(document).click(function(e){
     $('.register').animate({
     'margin-right':'-250',
- }, 'slow');
-});
+    }, 'slow');
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      var _self = $('.register');
+      $(_self).animate({
+      'margin-right':'-250',
+      }, 'slow');
+    }
+  }); // Press ESC will also close the drop-down
 });
