baaedc80149a3290b579e2830193ecc97c648079
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-17 19:46:43 +0100

    Final Changes v3

--- public/js/votes_jquery.js
+++ public/js/votes_jquery.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   $(document).on('click', '.vote-up', function(e) {
-    $(this).html(function(i, val){
+    $(this).html(function(val){
       var total =+ val + 1;
       $(this).text(' '+total);
     });
     e.preventDefault();
+    e.stopPropagation();
   });
   $(document).on('click', '.vote-down', function(e) {
-    $(this).html(function(i, val){
+    $(this).html(function(val){
       var total = val - 1;
       $(this).text(' '+total);
     });
     e.preventDefault();
+    e.stopPropagation();
   });
 });
